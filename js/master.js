a1cffeaf39f3715100938f67a367bc389d36f3e9
muhammed2378 <salnhmohammed123@gmail.com>
2023-02-20 22:18:33 +0300

    fixed the images path

--- js/master.js
+++ js/master.js
@@ -1,6 +1,6 @@
 
 // list of images sources
-let imgs = ['../images/img-1.jpg', '../images/img-2.jpg','../images/img-3.jpg', '../images//img-4.jpg', '../images/img-5.jpg', '../images/img-5.jpg', '../images/img-7.jpg', '../images/img-8.jpg', '../images/img-9.jpg','../images/img-10.jpg' ]
+let imgs = ['images/img-1.jpg', 'images/img-2.jpg','images/img-3.jpg', 'images//img-4.jpg', 'images/img-5.jpg', 'images/img-5.jpg', 'images/img-7.jpg', 'images/img-8.jpg', 'images/img-9.jpg','images/img-10.jpg' ]
 
 // check local storage
 
